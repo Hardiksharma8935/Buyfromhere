65b41bfdc5e06a5f7b69fb7a703289085c29aab2
Hardiksharma8935 <hardikrampur440@gmail.com>
2026-07-18 14:08:48 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,21 +1,19 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import SecretStr
-import os
 
 class Settings(BaseSettings):
     bot_token: SecretStr
     owner_id: int
     owner_username: str
-    main_channel: str
-    demo_channel: str
     database_url: str
-    
+    usd_to_inr_rate: float = 85.0
+    main_channel_link: str = "https://t.me/telegram"
+
+    # Crypto Wallets
+    usdt_address: str = "Not Configured"
     btc_address: str = "Not Configured"
     eth_address: str = "Not Configured"
     sol_address: str = "Not Configured"
-    usdt_address: str = "Not Configured"
-    stars_provider_token: str = ""
-    usd_to_inr_rate: float = 85.0
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
 
